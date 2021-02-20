87ad300b8bcbf2a7a8fb6d9e26f3e3ceb126476f
malwe <hans.martin1122@gmx.de>
2021-02-20 20:30:24 +0100

    I can't write

--- subMenus/skillMenu.js
+++ subMenus/skillMenu.js
@@ -2,6 +2,6 @@ class SkillMenu extends MenuTemplate {
   constructor(name, x, y, w, h, col) {
     super(name, x, y, w, h, col);
 
-    this.children.push(new Text("Current Skills", "player.skillPoints", 0, 0, 1, 0.1));
+    this.children.push(new Text("Current Skill Points: ", "player.skillPoints", 0, 0, 1, 0.1));
   }
 }
