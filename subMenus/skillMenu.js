2cc130b621b2dd778e11c0f797d80aa512f4d0d1
malwe <hans.martin1122@gmx.de>
2021-02-20 20:27:26 +0100

    instant hotfix

--- subMenus/skillMenu.js
+++ subMenus/skillMenu.js
@@ -2,7 +2,7 @@ class SkillMenu extends MenuTemplate {
   constructor(name, x, y, w, h, col) {
     super(name, x, y, w, h, col);
 
-    this.children.push(new Text("Current Skills", player.skillPoints, 0, 0, 1, 0.1));
+    this.children.push(new Text("Current Skills", "player.skillPoints", 0, 0, 1, 0.1));
     )
   }
 }
