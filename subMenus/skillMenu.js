0e6eb47450893dad96eb9146d701882e721ded54
malwe <hans.martin1122@gmx.de>
2021-02-21 13:41:41 +0100

    Preparing Skill Tab to work

--- subMenus/skillMenu.js
+++ subMenus/skillMenu.js
@@ -20,35 +20,30 @@ class SkillBlock extends MenuTemplate {
     super(name, x, y, w, h, col);
 
     this.children.push(new Button("-MAX", 0, 0, 0.075, 1, () => {
-      doStuff(100);
+      player.removeAttribute(att, -9999);
     }));
     this.children.push(new Button("-100", 0.075, 0, 0.075, 1, () => {
-      doStuff(10);
+      player.removeAttribute(att, -100);
     }));
     this.children.push(new Button("-10", 0.15, 0, 0.075, 1, () => {
-      doStuff(10);
+      player.removeAttribute(att, -10);
     }));
     this.children.push(new Button("-1", 0.225, 0, 0.075, 1, () => {
-      doStuff(10);
+      player.removeAttribute(att, -1);
     }));
     this.children.push(new Text(name + " Boost: ", "player.attributes."+att+".fromSkill", 0.3, 0, 0.2, 1));
     this.children.push(new Text("Level: ", "player.attributes."+att+".skillLevel", 0.5, 0, 0.2, 1));
     this.children.push(new Button("+1", 0.7, 0, 0.075, 1, () => {
-      doStuff(10);
+      player.addAttribute(att, 1);
     }));
     this.children.push(new Button("+10", 0.775, 0, 0.075, 1, () => {
-      doStuff(10);
+      player.addAttribute(att, 10);
     }));
     this.children.push(new Button("+100", 0.85, 0, 0.075, 1, () => {
-      doStuff(10);
+      player.addAttribute(att, 100);
     }));
     this.children.push(new Button("+MAX", 0.925, 0, 0.075, 1, () => {
-      doStuff(10);
+      player.addAttribute(att, 9999);
     }));
-
   }
-}
-
-function doStuff(v) {
-
 }
